5a5c92c5b42cc668e7484a2d5c2ad6272d8b68f7
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-09-05 15:57:16 +0900

    feat: initialize

---
+++ Backend/src/main/java/com/ktb/demo/QmakerApplication.java
@@ -0,0 +1,13 @@
+package com.ktb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QmakerApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/ktb/demo/QmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
