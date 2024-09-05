ae2623beceda9560ee2df7446e1d2e6ff48f4178
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-09-05 17:21:40 +0900

    feat: db connection 확인 완료

--- Backend/src/main/java/com/ktb/demo/QmakerApplication.java
+++ Backend/src/main/java/com/ktb/demo/QmakerApplication.java
@@ -2,8 +2,11 @@ package com.ktb.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableMongoRepositories
 public class QmakerApplication {
 
 	public static void main(String[] args) {
--- Backend/src/main/java/com/ktb/demo/controller/ProblemSetController.java
+++ Backend/src/main/java/com/ktb/demo/controller/ProblemSetController.java
@@ -3,6 +3,7 @@ package com.ktb.demo.controller;
 import com.ktb.demo.entity.TestInfo;
 import com.ktb.demo.service.ProblemSetService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,12 @@ public class ProblemSetController {
     private final ProblemSetService problemSetService;
 
     @PostMapping("/test-db")
-    public TestInfo testDb() {
+    public TestInfo testDb_save() {
         return problemSetService.save();
+    }
+
+    @GetMapping("/test-db")
+    public TestInfo testDb_find() {
+        return problemSetService.findById("1").orElse(null);
     }
 }
