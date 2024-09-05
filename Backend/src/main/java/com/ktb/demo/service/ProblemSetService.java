f5b4c063d9e8d134ad0fd51089fb311ab57bdd62
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-09-05 17:21:40 +0900

    feat: db connection 확인 완료

--- Backend/src/main/java/com/ktb/demo/service/ProblemSetService.java
+++ Backend/src/main/java/com/ktb/demo/service/ProblemSetService.java
@@ -3,6 +3,7 @@ package com.ktb.demo.service;
 import com.ktb.demo.entity.TestInfo;
 import com.ktb.demo.repository.ProblemSetRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,6 +13,7 @@ import java.util.Optional;
 public class ProblemSetService {
 
     private final ProblemSetRepository problemSetRepository;
+    private final MongoTemplate mongoTemplate;
 
     public TestInfo save() {
         return problemSetRepository.save(new TestInfo("1", "test1"));
