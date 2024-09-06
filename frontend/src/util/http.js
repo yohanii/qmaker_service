0b88761ad840f68379305dbe63818ce89f559703
KimMinWoooo <als33396dn@gmail.com>
2024-09-06 21:55:42 +0900

    fix : api endpoint 수정

--- frontend/src/util/http.js
+++ frontend/src/util/http.js
@@ -1,13 +1,16 @@
 // 노트 데이터 보내기
 export async function SendNoteData(textValue) {
   try {
-    const response = await fetch(`http://localhost:8080/problem-sets`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        note: textValue,
-      }),
-    });
+    const response = await fetch(
+      `http://backend-alb-1085769372.ap-northeast-2.elb.amazonaws.com/problem-sets`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          note: textValue,
+        }),
+      }
+    );
     const ProblemData = await response.json();
     return ProblemData;
   } catch (error) {
@@ -16,24 +19,26 @@ export async function SendNoteData(textValue) {
   }
 }
 
-
 // 채점 결과
 
-export async function AnswerData(answers){
-    const answerString = answers.join(',');
-    try {
-      const response = await fetch(`/problem-sets/S232424RWESD23/solved?answers=${answerString}`, {
+export async function AnswerData(answers) {
+  const answerString = answers.join(',');
+  try {
+    const response = await fetch(
+      `http://backend-alb-1085769372.ap-northeast-2.elb.amazonaws.com/problem-sets/S232424RWESD23/solved?answers=${answerString}`,
+      {
         method: 'GET',
-      });
-      
-      if (response.ok) {
-        const data = await response.json();
-        return data;
-      } else {
-        // 오류 처리
-        console.error('API 요청 실패');
       }
-    } catch (error) {
-      console.error('네트워크 오류:', error);
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      // 오류 처리
+      console.error('API 요청 실패');
     }
-  };
+  } catch (error) {
+    console.error('네트워크 오류:', error);
+  }
+}
