c69a31156f6a5babad3b045ba74288d2230c7d77
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-09-06 10:49:44 +0900

    feat: 채점 서비스 구현

--- Backend/src/main/java/com/ktb/demo/service/GradingProblemSetService.java
+++ Backend/src/main/java/com/ktb/demo/service/GradingProblemSetService.java
@@ -1,13 +1,16 @@
 package com.ktb.demo.service;
 
 import com.ktb.demo.dto.GradedProblemDto;
+import com.ktb.demo.entity.ProblemSet;
 import com.ktb.demo.repository.ProblemSetRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @RequiredArgsConstructor
@@ -17,7 +20,27 @@ public class GradingProblemSetService {
     private final MongoTemplate mongoTemplate;
 
     public List<GradedProblemDto> gradeProblemSet(String problemSetId, String answers) {
-        return new ArrayList<>();
+
+        List<Integer> userAnswers = splitEachAnswers(answers);
+
+        ProblemSet findProblemSet = problemSetRepository.findById(problemSetId)
+                .orElseThrow(() -> new NoSuchElementException("ProblemSet with id " + problemSetId + " not found"));
+
+        AtomicInteger index = new AtomicInteger();
+        return findProblemSet.getProblems().stream()
+                .map(problem -> new GradedProblemDto(
+                        problem.getAnswer() == userAnswers.get(index.getAndIncrement()),
+                        problem.getAnswer(),
+                        problem.getExplanation())
+                )
+                .toList();
+    }
+
+    private static List<Integer> splitEachAnswers(String answers) {
+        return Arrays.stream(answers.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
     }
 
 }
