18373023385ac65348729f5a5b66dded0d49244a
이미르 <mir@imileuui-MacBookPro.local>
2024-09-06 19:28:39 +0900

    feat: style 수정완

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,6 @@ function App() {
       <Route path="/" element={<Mainpage />}></Route>
       <Route path="quiz" element={<Quiz />}></Route>
       <Route path="answer" element={<Answer />}></Route>
-      <Route path="chart" element={<ChartResult />}></Route>
       </Routes>
       </BrowserRouter>
     </div>
